1ad0809db231004e2f48c92d8577b43353d4e0ff
Prometeo <105377032+Segismu@users.noreply.github.com>
2022-07-18 14:15:38 -0300

    Basic HP and Damage System

---
+++ Assets/Scripts/DamageController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class DamageController : MonoBehaviour
+{
+    [SerializeField] private int Damage;
+    [SerializeField] private HPController hpManager;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            DamageDealer();
+        }
+    }
+
+    void DamageDealer()
+    {
+        hpManager.playerHP = hpManager.playerHP - Damage;
+        hpManager.UpdateHP();
+
+        //Esto es provisorio. La idea es que los bichos vivan mas
+        //gameObject.SetActive(false);
+
+    }
+}
---
+++ Assets/Scripts/HPController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HPController : MonoBehaviour
+{
+    public int playerHP;
+
+    [SerializeField] Image[] hearts;
+
+    private void Start()
+    {
+        UpdateHP();
+    }
+
+    public void UpdateHP()
+    {
+
+        if (playerHP <= 0)
+        {
+            //Esto hay que cambiarlo a algo real cuando salga de test
+            SceneManager.LoadScene("TestPabli");
+        }
+            
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < playerHP)
+                {
+                    hearts[i].color = Color.red;
+                }
+            else
+            {
+                hearts[i].color = Color.black;
+            }
+        }
+    }
+}
