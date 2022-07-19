39f1d3c93289b32be5fec06fbd836689d0e6889b
Prometeo <105377032+Segismu@users.noreply.github.com>
2022-07-19 16:14:45 -0300

    Minor prefab changes. Added Clock and Explosion Particles.

--- Assets/Scripts/DamageController.cs
+++ Assets/Scripts/DamageController.cs
@@ -1,25 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DamageController : MonoBehaviour
 {
     [SerializeField] private int Damage;
     [SerializeField] private HPController hpManager;
+    [SerializeField] ParticleSystem bugExplosion;
+    [SerializeField] float deathDelay = 1f;
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            bugExplosion.Play();
             DamageDealer();
         }
     }
 
     void DamageDealer()
     {
+        
         hpManager.playerHP = hpManager.playerHP - Damage;
         hpManager.UpdateHP();
+        Invoke("EnemyEraser", deathDelay);
 
         //Esto es provisorio. La idea es que los bichos vivan mas
-        //gameObject.SetActive(false);
 
+
+    }
+
+    void EnemyEraser()
+    {
+        
+        Destroy(gameObject);
     }
 }
