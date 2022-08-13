9b368a04ca32f5609df7d035b89e4cd6d3639945
Prometeo <105377032+Segismu@users.noreply.github.com>
2022-08-13 14:45:53 -0300

    Added key and gate logic.

---
+++ Assets/Scripts/KeyHolder.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class KeyHolder : MonoBehaviour
+{
+    public event EventHandler OnKeysChanged;
+
+    private List<Key.KeyType> keyList;
+
+    private void Awake()
+    {
+        keyList = new List<Key.KeyType>();
+    }
+
+    public List<Key.KeyType> GetKeyList()
+    {
+        return keyList;
+    }
+
+    public void AddKey(Key.KeyType keyType)
+    {
+        Debug.Log("Added key" + keyType);
+        keyList.Add(keyType);
+        OnKeysChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public void RemoveKey(Key.KeyType keyType)
+    {
+        keyList.Remove(keyType);
+        OnKeysChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public bool HasKey(Key.KeyType keyType)
+    {
+        return keyList.Contains(keyType);
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        Key key = collider.GetComponent<Key>();
+        if (key != null)
+        {
+            AddKey(key.GetKeyType());
+            Destroy(key.gameObject);
+        }
+
+        KeyGate keyGate = collider.GetComponent<KeyGate>();
+        if (keyGate != null)
+        {
+            if (HasKey(keyGate.GetKeyType()))
+            {
+                RemoveKey(keyGate.GetKeyType());
+                keyGate.OpenGate();
+            }
+        }
+
+    }
+}
