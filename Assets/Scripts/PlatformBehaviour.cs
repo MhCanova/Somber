4265f7a16893416fee06841283bf8be8e993a4d1
Prometeo <105377032+Segismu@users.noreply.github.com>
2022-08-16 12:00:09 -0300

    Fixed plataforms.

--- Assets/Scripts/PlatformBehaviour.cs
+++ Assets/Scripts/PlatformBehaviour.cs
@@ -5,27 +5,21 @@ using UnityEngine;
 public class PlatformBehaviour : MonoBehaviour
 {
     
-void OnCollisionEnter(Collision col)
-{
-    
-    if(col.gameObject.tag == "Platform")
+    public GameObject Player;
+
+    private void OnTriggerEnter(Collider other)
     {
-        //col.gameObject.transform.SetParent(gameObject.transform, true);
-        transform.parent = col.gameObject.transform;
-        Debug.Log("We are on a moving platform");
+        if(other.gameObject == Player)
+        {
+            Player.transform.parent = transform;
+        }
     }
 
-}
-
-void OnCollisionExit(Collision col)
-{
-    
-    
-    if(col.gameObject.tag == "Platform")
+    private void OnTriggerExit(Collider other)
     {
-        //col.gameObject.transform.parent = null;
-        transform.parent = null;
+        if(other.gameObject == Player)
+        {
+            Player.transform.parent = null;
+        }
     }
-
-}
 }
